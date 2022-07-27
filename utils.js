ce8257c3505fca307fcb550aa7f5f6baa5da5503
didier-ds <didiersenou94@gmail.com>
2022-07-27 17:44:58 +0100

    setup

---
+++ utils.js
@@ -0,0 +1,8 @@
+const getActiveTabUrl = async () => {
+    const queryOptions = { active: true, lastFocusedWindow: true };
+    // `tab` will either be a `tabs.Tab` instance or `undefined`.
+    let [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+  }
+
+  export { getActiveTabUrl }
